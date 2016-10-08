b28035c8561f37bb1e1a90dd9ca401f7cbcba5a2
Francisco Garcia <fgarcia.lopez@hotmail.com>
2016-10-08 09:57:40 -0600

    segundo codigo

--- src/parametros_posicionales.sh
+++ src/parametros_posicionales.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#La salida estandar....
-if cp archivo1.txt archivo1.txt.bck 2> $0.err
+ERRORLOG="$0.err"
+#La salida estandar del error estara redireccionada al archivo if-then.sh.err
+if cp archivo1.txt archivo1.txt.bck 2> $ERRORLOG    
 then
     echo"El archivo.txt fue correctamente respaldado..."
+else
+    echo"El archivo1.txt no pudo ser respaldado, verifique el log: $errorlog"
 fi
