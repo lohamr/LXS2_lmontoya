4b2e7177ab628829f2981bd38b88afdda3b3b491
Francisco Garcia <fgarcia.lopez@hotmail.com>
2016-10-08 11:14:10 -0600

    tercer codigo if, elif

---
+++ src/parametros_posicionales_avanzados.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+if test -e $1 $1.bck
+then
+	cd $1 $1.bck
+	echo "el archivo $1 fue correctamente respaldado..."
+elif test -e $2
+then
+	cp $2 $2.bck
+	echo  "el archivo $2 se resplado correctamente"
+ 
+elif test -e $3
+then
+	cp $3 $3.bck
+	echo  "el archivo $3 se resplado correctamente"
+else
+	echo "no se pudo respaldar ninguno de los archivos> $@"
+fi
