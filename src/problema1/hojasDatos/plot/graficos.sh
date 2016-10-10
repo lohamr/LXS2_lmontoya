cec24b31e73254142ed593102afd0186a172bad6
lohana <loha.montoya@gmail.com>
2016-10-10 02:22:21 -0600

    intento de realizar primer gráfico, ./graficos.sh

---
+++ src/problema1/hojasDatos/plot/graficos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+FMT_BEGIN=20161010 0220
+FMT_END=20161010 0223
+FMT_X_SHOW=%m/%d
+
+
+graficar()
+{
+	gnuplot << EOF 2> error.log
+	set xdata time
+	set timefmt "%Y%m%d%H%M"
+	set xrange ["$FMT_BEGIN" : "$FMT_END"]
+	set format x "$FMT_X_SHOW"
+	set terminal png
+	set output 'fig1.pgn'
+	plot "graf-0.dat" using 1:3 with lines title "sensor1","graf-0.dat" using 1:4 with linespoints title "sensor2"
+EOF
+}
+graficar
+
+m=0
+for k in 'find $GRAF_DATA -name "*.dat"'
+do
+	sed '1d' $k >> $FULL_DATA/full.dat
+	let m=m+1
+done 2> error3.log
