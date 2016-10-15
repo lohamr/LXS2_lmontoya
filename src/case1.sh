a17f22b9cefd6558e27207a43d7c77f50626a544
Lohana Montoya <loha.montoya@gmail.com>
2016-10-15 10:01:16 -0600

    case avanzada

--- src/case1.sh
+++ src/case1.sh
@@ -5,22 +5,34 @@
 shopt -s nocasematch
 
 DISTRO=$1
+VAR2=$2
+VAR3=$3
 
 #Las funciones se deben definir antes de ser llamadas.
 mensaje ()
 {
 echo "Distrubicion $1 soportada"
+echo "Distrubicion $2 soportada"
+echo "Distrubicion $3 soportada"
+
+CONTADOR=1
+
+for i in $@
+do
+	echo "El parametro de la funcion fueron $i"
+	let "CONTADOR=$CONTADOR+1"
+done
 }
 
 case "$DISTRO" in
 	Ubuntu)
-		mensaje $DISTRO
+		mensaje $DISTRO $VAR2 $VAR3
 	;;
 	Centos)
-		mensaje $DISTRO
+		mensaje $DISTRO $VAR2 $VAR3
 	;;
 	Fedora)
-		mensaje $DISTRO
+		mensaje $DISTRO $VAR2 $VAR3
 	;;
 	*)
 		echo "Distribucion no soportada"
