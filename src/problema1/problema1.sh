9ef54022b7730627159449067df89cdc29cc088e
lohana <loha.montoya@gmail.com>
2016-10-17 18:31:01 -0600

    Lo logré

--- src/problema1/problema1.sh
+++ src/problema1/problema1.sh
@@ -59,13 +59,12 @@ graficar()
 {
 	gnuplot << EOF 2> error.log
 	set xdata time
-	set timefmt "%Y%m%d%H%M"
-#	set xrange ["$FMT_BEGIN" : "$FMT_END"]
+	set timefmt "%Y%m%d %H%M"
+	set xrange ["$FMT_BEGIN" : "$FMT_END"]
 	set format x "$FMT_X_SHOW"
 	set terminal png
 	set output 'fig1.png'
-	plot "$DATA_DONE" using 1:3 with lines title "sensor1"
-#	,"$DATA_DONE" using 1:4 with linespoints title "sensor2"
+	plot "$DATA_DONE" using 1:3 with lines title "sensor1","$DATA_DONE" using 1:4 with linespoints title "sensor2"
 EOF
 
 }
